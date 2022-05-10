e4db9d187771db16a3523bdd03cbe9fb592e366c
rnaid <rnaid@192.168.0.109>
2022-05-10 19:57:08 +0200

    Added skeleton for the 2D array creation function.

--- src/main/java/myprojects/slidingblockpuzzle/SlidingBlockPuzzleUtils.java
+++ src/main/java/myprojects/slidingblockpuzzle/SlidingBlockPuzzleUtils.java
@@ -10,6 +10,16 @@ package myprojects.slidingblockpuzzle;
  */
 public class SlidingBlockPuzzleUtils {
     
+    /*
+    This Function creates and returned a randomized 2D array that contains the tiles ranging 
+    from 1 to 15, with a 0 representing the impty block.
+    */
+    public int[][] CreateArray(int rows, int cols){
+        int[][] slidingBlockPuzzle = new int[rows][cols];
+        //Fill array here
+        return slidingBlockPuzzle;
+    }
+    
     public static void main(String[] args){
         
     }
