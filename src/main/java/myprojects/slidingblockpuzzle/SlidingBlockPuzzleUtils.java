05bfde612fd28123152fe69554911f9ba725e7d9
rnaid <rnaid@192.168.0.109>
2022-05-11 12:04:55 +0200

    Created function to create a new randomized 2D array (CreateArray), added a function to test if an integer is already present in the 2D array (CheckArray), and added some comments.

--- src/main/java/myprojects/slidingblockpuzzle/SlidingBlockPuzzleUtils.java
+++ src/main/java/myprojects/slidingblockpuzzle/SlidingBlockPuzzleUtils.java
@@ -9,33 +9,75 @@ package myprojects.slidingblockpuzzle;
  * @author rnaid
  */
 public class SlidingBlockPuzzleUtils {
-    
+
     /*
     This Function creates and returned a randomized 2D array that contains the tiles ranging 
     from 1 to 15, with a 0 representing the impty block.
-    */
-    public static int[][] CreateArray(int rows, int cols){
+     */
+    public static int[][] CreateArray(int rows, int cols) {
         int[][] slidingBlockPuzzle = new int[rows][cols];
-        //Fill array here
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                slidingBlockPuzzle[i][j] = (rows * cols) + 1;
+            }
+        }
+        boolean isNewInt = false;
+        int toAdd;
+        int toCheck;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                while (!isNewInt) {
+                    toAdd = (int) (((Math.random()) * 16) + 1);
+                    isNewInt = NewNumber(toAdd, slidingBlockPuzzle);
+                }
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(slidingBlockPuzzle[i][j]==16){
+                    slidingBlockPuzzle[i][j] = 0;
+                }
+            }
+        }
         return slidingBlockPuzzle;
     }
-    
+
+    /*
+    This function is called to check if a number is already present in the 2D array.
+    It returns false if this is the case, else it returns true.
+    */
+    public static boolean NewNumber(int toCheck, int[][] slidingBlockPuzzle) {
+        boolean isNewNumber = true;
+        int rows = slidingBlockPuzzle.length;
+        int cols = slidingBlockPuzzle[0].length;
+        int current;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                current = slidingBlockPuzzle[i][j];
+                if (toCheck == current) {
+                    isNewNumber = false;
+                }
+            }
+        }
+        return isNewNumber;
+    }
+
     /*
     This function accepts the initial 2D array and solves it on a step-by-step basis.
-    */
-    public static void SolvePuzzle(int[][] slidingBlockPuzzle){
-        
+     */
+    public static void SolvePuzzle(int[][] slidingBlockPuzzle) {
+
     }
-    
+
     /*
     This function prints the contents of a 2D array to the output screen.
-    */
-    public static void PrintArray(int[][] slidingBlockPuzzle){
+     */
+    public static void PrintArray(int[][] slidingBlockPuzzle) {
         
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         
     }
-    
+
 }
